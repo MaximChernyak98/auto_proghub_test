7d5ac9ea17267959884501246c402f2632742e77
mecher <maximchernyak98@gmail.com>
2020-12-05 16:47:46 +0300

    can work in Widows and Linux

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 import pytest
+import platform
 
 
 @pytest.fixture()
 def setup():
-    chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--window-size=1420,1080')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = None
+    if platform.system() == 'Windows':
+        driver = webdriver.Chrome()
+    else:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(chrome_options=chrome_options)
     return driver
